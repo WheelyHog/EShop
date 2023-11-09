2e44ea065556fbca5929d9bb37dcd35621d33fe1
Wheelyhog <wheelyhog@gmail.com>
2023-11-09 20:14:18 +0100

    use strapi

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,42 +1,63 @@
 import React from 'react'
 import "./featured.scss"
 import Card from '../Card/Card'
+import useFetch from '../../hooks/useFetch'
 
-const data = [
-    {
-        id: 1,
-        img: "https://images.unsplash.com/photo-1621072156002-e2fccdc0b176?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8U2hpcnR8ZW58MHx8MHx8fDA%3D",
-        img2: "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGphY2tldHxlbnwwfHwwfHx8MA%3D%3D",
-        title: "Shirt",
-        isNew: true,
-        oldPrice: 19,
-        price: 12
-    },
-    {
-        id: 2,
-        img: "https://images.unsplash.com/photo-1594633313515-7ad9334a2349?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2tpcnRzfGVufDB8fDB8fHww",
-        title: "Skirt",
-        isNew: true,
-        oldPrice: 19,
-        price: 12
-    },
-    {
-        id: 3,
-        img: "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGphY2tldHxlbnwwfHwwfHx8MA%3D%3D",
-        title: "Jacket",
-        oldPrice: 19,
-        price: 12
-    },
-    {
-        id: 4,
-        img: "https://images.unsplash.com/photo-1514327605112-b887c0e61c0a?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGF0fGVufDB8fDB8fHww",
-        title: "Hat",
-        oldPrice: 19,
-        price: 12
-    },
-]
+// const data = [
+//     {
+//         id: 1,
+//         img: "https://images.unsplash.com/photo-1621072156002-e2fccdc0b176?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8U2hpcnR8ZW58MHx8MHx8fDA%3D",
+//         img2: "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGphY2tldHxlbnwwfHwwfHx8MA%3D%3D",
+//         title: "Shirt",
+//         isNew: true,
+//         oldPrice: 19,
+//         price: 12
+//     },
+//     {
+//         id: 2,
+//         img: "https://images.unsplash.com/photo-1594633313515-7ad9334a2349?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2tpcnRzfGVufDB8fDB8fHww",
+//         title: "Skirt",
+//         isNew: true,
+//         oldPrice: 19,
+//         price: 12
+//     },
+//     {
+//         id: 3,
+//         img: "https://images.unsplash.com/photo-1608063615781-e2ef8c73d114?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGphY2tldHxlbnwwfHwwfHx8MA%3D%3D",
+//         title: "Jacket",
+//         oldPrice: 19,
+//         price: 12
+//     },
+//     {
+//         id: 4,
+//         img: "https://images.unsplash.com/photo-1514327605112-b887c0e61c0a?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aGF0fGVufDB8fDB8fHww",
+//         title: "Hat",
+//         oldPrice: 19,
+//         price: 12
+//     },
+// ]
 
 const FeaturedProducts = ({ type }) => {
+
+    const { data, loading, error } = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const res = await axios.get(REACT_APP_API_URL + `/products?populate=*&[filters][type][$eq]=${type}`, {
+    //                 headers: {
+    //                     Authorization: "bearer " + REACT_APP_API_TOKEN
+    //                 }
+    //             })
+    //             setData(res.data.data)
+    //         } catch (error) {
+    //             console.log(error);
+    //         }
+    //     }
+    //     fetchData()
+    // }, [])
+
+
     return (
         <div className='featured'>
             <div className="top">
@@ -50,9 +71,12 @@ const FeaturedProducts = ({ type }) => {
                 </p>
             </div>
             <div className="bottom">
-                {data?.map(item => (
-                    <Card item={item} key={item.id} />
-                ))}
+                {error
+                    ? "Something went wrong!"
+                    : loading
+                        ? "loading"
+                        : data?.map((item) => <Card item={item} key={item.id} />
+                        )}
             </div>
         </div>
     )
---
+++ src/makeRequest.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { REACT_APP_API_TOKEN, REACT_APP_API_URL } from "./request";
+
+export const makeRequest = axios.create({
+  baseURL: REACT_APP_API_URL,
+  headers: {
+    Authorization: "bearer " + REACT_APP_API_TOKEN,
+  },
+});
